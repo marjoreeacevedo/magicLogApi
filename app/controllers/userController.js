ef191f72732662d75d52ec38c7985adb462a5d99
Marjoree Acevedo <marjoree.acevedo@detektor.com.hn>
2023-07-23 21:41:12 -0600

    Ajuste registro

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -18,19 +18,19 @@ class userController {
 
    register(req, res) {
         const { mail, pass} = req.body;
-        let query =`insert into user
-        (id,mail, pass) values(null,'${mail}','${pass}')`;
+        let query =`insert into usuario
+        (mail, pass) values('${mail}','${pass}')`;
         connection.conn().query(query, (err, result) => {
             if (err) {
                 return res.status(400).send({
                     status: "Error",
-                    description: 'Error al consultar la placas. Error: ' + err.message
+                    description: 'Error al consultar los usuarios Error: ' + err.message
                 });
             } else {
         return res.status(200).send({
             status: "Success",
             data: 'Usuario insertado correctamente'
-                    });
+             });
             }
 
         });
